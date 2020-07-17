999811fbc3e86565740d7c6562ea3c25231f0a59
irfan1991 <irfanprasetyo91@gmail.com>
2020-07-17 10:50:44 +0700

    grid produk

--- src/scenes/private/produk/grid.js
+++ src/scenes/private/produk/grid.js
@@ -9,6 +9,11 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
+import ImageIcon from "@material-ui/icons/Image";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+
 
 // mport style
 import useStyles from './style/grid';
@@ -21,10 +26,17 @@ import AppLoading from "../../../components/AppLoading";
 import { useFirebase } from "../../../components/FirebaseProvider";
 import {useCollection} from 'react-firebase-hooks/firestore';
 
+//import helper
+import { currency } from "../../../helpers/formatter";
+
+//import react router
+import { Link } from 'react-router-dom';
+
+
 export default function GridProduk() {
 
     const classes = useStyles();
-    const { firestore , user} = useFirebase();
+    const { firestore , user, storage } = useFirebase();
     const produkCol = firestore.collection(`toko/${user.uid}/produk`);
     const [snapshot , loading] = useCollection(produkCol);
     const [produkItems, setProdukItems] = useState([]);
@@ -38,6 +50,16 @@ export default function GridProduk() {
         }
 
     },[snapshot]);
+
+    const handleDelete = produkDoc => async e => {
+        if(window.confirm(`Anda yakin menghapus data ini ? `)){
+            await produkDoc.ref.delete();
+            const fotoUrl = produkDoc.data().foto;
+            if(fotoUrl) {
+                await storage.refFromURL(fotoUrl).delete();
+            }
+        }
+    }
 
     if(loading){
         return <AppLoading />
@@ -60,11 +82,37 @@ export default function GridProduk() {
 
                     const produkData = produkDoc.data();
                     return  <Grid key={produkData.id} item={true} xs={12} sm={12} md={6} lg={4}>
-                            <Card>
-                                <CardContent>
-                                     <Typography variant="h5" noWarp>{produkData.nama}</Typography>
+                            <Card className={classes.card}>
+                                {
+                                    produkData.foto &&
+                                    <CardMedia className={classes.foto}
+                                        image={produkData.foto}
+                                        title={produkData.nama}
+                                    />
+                                }
 
+                                {
+                                    !produkData.foto &&
+                                    <div className={classes.fotoPlaceholder}>
+                                        <ImageIcon />
+                                    </div>
+                                }
+                                <CardContent className={classes.produkDetails}>
+                                     <Typography variant="h5" noWarp>{produkData.nama}</Typography>
+                                     <Typography variant="subtitle1"> Harga : {currency(produkData.harga)}</Typography>
+                                     <Typography > Stock : {produkData.stock}</Typography>
                                 </CardContent>
+                                <CardActions className={classes.produkActions}>
+                                    <IconButton component = {Link} to={`/produk/edit/${produkDoc.id}`}>
+                                         <EditIcon />
+                                    </IconButton>
+                                    <IconButton
+                                        onClick={handleDelete(produkDoc)}
+                                    >
+                                        <DeleteIcon />
+                                    </IconButton>
+                                   
+                                </CardActions>
                             </Card>
 
                         </Grid>
